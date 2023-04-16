673a73cc639dedab6cd3f458172622730f39fddf
dicksonmutwiriguantai <dickson.mutwiri@student.moringaschool.com>
2023-04-16 16:26:26 +0300

    Completed Assignement

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+let name = "Dickson"
+let email = "dicksonguantai@gmail.com"
+
+
+function submitData(name,email){
+// const userDetails = {name,email};
+
+const postUserDetails = {
+    method:'POST',
+    headers:{
+        "Content-Type": "application/json",
+        Accept: "application/json"
+    },
+    body:JSON.stringify({
+        name:`${name}`,
+        email:`${email}`
+    }),
+};
+return( fetch("http://localhost:3000/users",postUserDetails)
+    .then(res =>res.json())
+    .then(data => {
+        
+    document.body.textContent= data.id
+
+})
+    .catch(error =>{
+    document.body.textContent= error.message
+}))
+}
